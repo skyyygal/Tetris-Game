b8ecf7275168b5f7748b304b3bf038f7e61eb969
skyyygal <heeramnakp@gmail.com>
2025-11-03 23:18:00 +0530

    Initial commit

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -39,7 +39,40 @@ class _GameBoardState extends State<GameBoard> {
   //return true -> collision detected
   //return false -> no collision
 
-  // bool checkCollision(Direction direction) {}
+  bool checkCollision(Direction direction) {
+    for (int i = 0; i < currentPiece.positions.length; i++) {
+      int row = (currentPiece.positions[i] / rowCount).floor();
+      int col = currentPiece.positions[i] % rowCount;
+
+      switch (direction) {
+        case Direction.left:
+          col -= 1;
+          // if (column == 0) {
+          //   return true;
+          // }
+          break;
+        case Direction.right:
+          col += 1;
+          // if (column == rowCount - 1) {
+          //   return true;
+          // }
+          break;
+        case Direction.down:
+          row += 1;
+          // if (row == columnCount - 1) {
+          //   return true;
+          // }
+          break;
+      }
+      // check if the piece is out of bounds
+      if (row >= columnCount || col < 0 || col >= rowCount) {
+        return true;
+      }
+      //if not collisions are detected, return false
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
